ba05855f10369f67cae76bfaee6d9901ef7c7915
vitket <skolkojvastut@gmail.com>
2019-07-11 16:05:39 +0300

    Start empty project

---
+++ MapsManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapsManager
+//
+//  Created by vit on 11/07/2019.
+//  Copyright © 2019 vit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
